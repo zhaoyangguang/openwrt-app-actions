84c815c812e4fb4f13637f2e3234b619a42cebd6
yangguang_ymette <zhaoyangguang@foxmail.com>
2023-12-04 13:19:06 +0800

    测试

---
+++ applications/luci-app-bmtedge/root/usr/libexec/istorec/rebatedog.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+# Author Xiaobao(xiaobao@linkease.com)
+
+ACTION=${1}
+shift 1
+
+IMAGE_NAME='zhaoyangguang/rebatedog:latest'
+
+
+do_install() {
+  local http_port=`uci get rebatedog.@rebatedog[0].port 2>/dev/null`
+  local path=`uci get rebatedog.@rebatedog[0].data_path 2>/dev/null`
+  [ -z $http_port ] || http_port=15888
+
+
+  echo "docker pull ${IMAGE_NAME}"
+  docker pull ${IMAGE_NAME}
+  docker rm -f rebatedog
+
+  local cmd="docker run --restart=unless-stopped -d \
+    --dns=172.17.0.1 \
+    --dns=114.114.114.114 \
+ --network=host \
+	  -v \"$path:/app/data\" \
+    -p ${http_port}:15888 "
+
+  local tz="`uci get system.@system[0].zonename`"
+  [ -z "$tz" ] || cmd="$cmd -e TZ=$tz"
+
+  cmd="$cmd -v /mnt:/mnt"
+  mountpoint -q /mnt && cmd="$cmd:rslave"
+  cmd="$cmd --name rebatedog \"$IMAGE_NAME\""
+
+  echo "$cmd"
+  eval "$cmd"
+}
+
+usage() {
+  echo "usage: $0 sub-command"
+  echo "where sub-command is one of:"
+  echo "      install                Install the Rebatedog"
+  echo "      upgrade                Upgrade the Rebatedog"
+  echo "      rm/start/stop/restart  Remove/Start/Stop/Restart the Rebatedog"
+  echo "      status                 Rebatedog status"
+  echo "      port                   Rebatedog port"
+}
+
+case ${ACTION} in
+  "install")
+    do_install
+  ;;
+  "upgrade")
+    do_install
+  ;;
+  "rm")
+    docker rm -f rebatedog
+  ;;
+  "start" | "stop" | "restart")
+    docker ${ACTION} rebatedog
+  ;;
+  "status")
+    docker ps --all -f 'name=rebatedog' --format '{{.State}}'
+  ;;
+  "port")
+    docker ps --all -f 'name=rebatedog' --format '{{.Ports}}' | grep -om1 '0.0.0.0:[0-9]*' | sed 's/0.0.0.0://'
+  ;;
+  *)
+    usage
+    exit 1
+  ;;
+esac
